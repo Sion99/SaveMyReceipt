cb10695ae94f8d69b0920d55716675e361653567
sion99 <kmicety1@gmail.com>
2023-08-10 00:26:10 +0900

    .

--- receipt.js
+++ receipt.js
@@ -10,9 +10,43 @@ closeModalBtn.addEventListener('click', () => {
 	container.innerHTML = '';
 })
 
-realSubmitBtn.onclick = uploadFile;
+realSubmitBtn.onclick = sendImageToEC2;
 
-function uploadFile() {
+async function sendImageToEC2() {
+	const ec2Url = 'http://ec2-13-124-154-116.ap-northeast-2.compute.amazonaws.com:8000/post'; // EC2 인스턴스의 URL로 대체
+
+	// Create a FormData object and append the image file
+	const formData = new FormData();
+	formData.append('image', fileInput.files[0]);
+
+	try {
+		const response = await fetch(ec2Url, {
+			method: 'POST',
+			body: formData,
+		});
+
+		if (response.ok) {
+			const responseData = await response.json();
+			console.log('Response from EC2:', responseData);
+		} else {
+			console.error('Error sending image to EC2:', response.statusText);
+		}
+	} catch (error) {
+		console.error('Error:', error);
+	}
+}
+
+// // Usage example:
+// const inputFile = document.querySelector('#imageInput'); // Assuming you have an input element with type="file"
+// inputFile.addEventListener('change', (event) => {
+// 	const selectedFile = event.target.files[0];
+// 	if (selectedFile) {
+// 		sendImageToEC2(selectedFile);
+// 	}
+// });
+
+
+async function uploadFile() {
 	const formData = new FormData;
 	var file = fileInput.files[0];
 	formData.append('file', file);
